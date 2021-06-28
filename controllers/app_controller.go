825a75f64425254e8c60d6b9b3c574e17477d47e
徐植君 <tony121.xu@huolala.cn>
2021-06-28 09:35:54 +0800

    使用operator实现应用快速k8s部署

---
+++ controllers/app_controller.go
@@ -0,0 +1,152 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/xuzhijvn/app/resource/deployment"
+	"github.com/xuzhijvn/app/resource/service"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"reflect"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	appv1 "github.com/xuzhijvn/app/api/v1"
+)
+
+// AppReconciler reconciles a App object
+type AppReconciler struct {
+	client.Client
+	Log    logr.Logger
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=app.huolala.cn,resources=apps,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=app.huolala.cn,resources=apps/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=app.huolala.cn,resources=apps/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the App object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
+func (r *AppReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = r.Log.WithValues("app", req.NamespacedName)
+
+	// your logic here
+
+	// 获取 crd 资源
+	instance := &appv1.App{}
+	if err := r.Client.Get(ctx, req.NamespacedName, instance); err != nil {
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	// crd 资源已经标记为删除
+	if instance.DeletionTimestamp != nil {
+		return ctrl.Result{}, nil
+	}
+
+	oldDeploy := &appsv1.Deployment{}
+	if err := r.Client.Get(ctx, req.NamespacedName, oldDeploy); err != nil {
+		// deployment 不存在，创建
+		if errors.IsNotFound(err) {
+			// 创建deployment
+			if err := r.Client.Create(ctx, deployment.New(instance)); err != nil {
+				return ctrl.Result{}, err
+			}
+
+			// 创建service
+			if err := r.Client.Create(ctx, service.New(instance)); err != nil {
+				return ctrl.Result{}, err
+			}
+
+			// 更新 crd 资源的 Annotations
+			data, _ := json.Marshal(instance.Spec)
+			if instance.Annotations != nil {
+				instance.Annotations["spec"] = string(data)
+			} else {
+				instance.Annotations = map[string]string{"spec": string(data)}
+			}
+			if err := r.Client.Update(ctx, instance); err != nil {
+				return ctrl.Result{}, err
+			}
+		} else {
+			return ctrl.Result{}, err
+		}
+	} else {
+		// deployment 存在，更新
+		oldSpec := appv1.AppSpec{}
+		if err := json.Unmarshal([]byte(instance.Annotations["spec"]), &oldSpec); err != nil {
+			return ctrl.Result{}, err
+		}
+
+		if !reflect.DeepEqual(instance.Spec, oldSpec) {
+			// 更新deployment
+			newDeploy := deployment.New(instance)
+			oldDeploy.Spec = newDeploy.Spec
+			if err := r.Client.Update(ctx, oldDeploy); err != nil {
+				return ctrl.Result{}, err
+			}
+
+			// 更新service
+			newService := service.New(instance)
+			oldService := &corev1.Service{}
+			if err := r.Client.Get(ctx, req.NamespacedName, oldService); err != nil {
+				return ctrl.Result{}, err
+			}
+			clusterIP := oldService.Spec.ClusterIP // 更新 service 必须设置老的 clusterIP
+			oldService.Spec = newService.Spec
+			oldService.Spec.ClusterIP = clusterIP
+			if err := r.Client.Update(ctx, oldService); err != nil {
+				return ctrl.Result{}, err
+			}
+
+			// 更新 crd 资源的 Annotations
+			data, _ := json.Marshal(instance.Spec)
+			if instance.Annotations != nil {
+				instance.Annotations["spec"] = string(data)
+			} else {
+				instance.Annotations = map[string]string{"spec": string(data)}
+			}
+			if err := r.Client.Update(ctx, instance); err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+	}
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *AppReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&appv1.App{}).
+		Complete(r)
+}
---
+++ resource/deployment/deployment.go
@@ -0,0 +1,62 @@
+package deployment
+
+import (
+	appv1 "github.com/xuzhijvn/app/api/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+func New(app *appv1.App) *appsv1.Deployment {
+	labels := map[string]string{"app.example.com/v1": app.Name}
+	selector := &metav1.LabelSelector{MatchLabels: labels}
+	return &appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "apps/v1",
+			Kind:       "Deployment",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      app.Name,
+			Namespace: app.Namespace,
+			OwnerReferences: []metav1.OwnerReference{
+				*metav1.NewControllerRef(app, schema.GroupVersionKind{
+					Group:   appv1.GroupVersion.Group,
+					Version: appv1.GroupVersion.Version,
+					Kind:    "App",
+				}),
+			},
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: app.Spec.Replicas,
+			Selector: selector,
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: labels,
+				},
+				Spec: corev1.PodSpec{
+					Containers: newContainers(app),
+				},
+			},
+		},
+	}
+}
+
+func newContainers(app *appv1.App) []corev1.Container {
+	var containerPorts []corev1.ContainerPort
+	for _, servicePort := range app.Spec.Ports {
+		var cport corev1.ContainerPort
+		cport.ContainerPort = servicePort.TargetPort.IntVal
+		containerPorts = append(containerPorts, cport)
+	}
+	return []corev1.Container{
+		{
+			Name:            app.Name,
+			Image:           app.Spec.Image,
+			Ports:           containerPorts,
+			Env:             app.Spec.Envs,
+			Resources:       app.Spec.Resources,
+			ImagePullPolicy: corev1.PullIfNotPresent,
+		},
+	}
+}
